85d953c26d79df59436bce10e5eb9f49b6c88416
Jadson Medeiros <developer.medeiros@gmail.com>
2020-06-03 23:46:49 -0300

    first push

---
+++ src/controllers/ItemsController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import knex from "../database/connection";
+
+class ItemsController {
+  async index(request: Request, response: Response) {
+    const items = await knex("items").select("*");
+    const itemsWithImageUrl = items.map((item) => {
+      item.image_url = `http://localhost:3333/uploads/${item.image}`;
+      return item;
+    });
+    response.json(itemsWithImageUrl);
+  }
+}
+
+export default ItemsController;
