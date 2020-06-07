d58a86b581e8e4e88d440db2253d37507043f2f9
Jadson Medeiros <developer.medeiros@gmail.com>
2020-06-06 22:38:13 -0300

    add routes and upload image

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,22 @@
 import express from "express";
 
-const routes = express.Router();
+import multer from "multer";
+import multerConfig from "./config/multer";
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
+
+const routes = express.Router();
+const upload = multer(multerConfig);
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get("/items", itemsController.index);
+
+routes.get("/points", pointsController.index);
+routes.get("/points/:id", pointsController.show);
+
+routes.post("/points", upload.single("image"), pointsController.create);
 
 export default routes;
